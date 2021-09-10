93eafe5ff309b6fd3b7177a10f01d100646bf107
Ahmet Aslan <aslanahmet@Ahmets-MacBook-Pro.local>
2021-09-10 14:55:09 +0300

    TEST

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,32 +6,45 @@ import {Helmet} from "react-helmet";
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi Ahmet</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-    <Helmet>
-        <script src="https://use.typekit.net/foobar.js"></script>
-    </Helmet>
-  </Layout>
-)
+class ApiCall extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todo: {}
+        };
+    }
 
-window.insPageShow = function () {
-    console.log('test');
+    componentWillMount() {
+        console.log('ahmet son');
+    }
+
+    render() {
+        const { todo } = this.state;
+        console.log(todo)
+        return (
+            <Layout>
+                <Seo title="Home" />
+                <h1>Hi Ahmet</h1>
+                <p>Welcome to your new Gatsby site.</p>
+                <p>Now go build something great.</p>
+                <StaticImage
+                    src="../images/gatsby-astronaut.png"
+                    width={300}
+                    quality={95}
+                    formats={["auto", "webp", "avif"]}
+                    alt="A Gatsby astronaut"
+                    style={{ marginBottom: `1.45rem` }}
+                />
+                <p>
+                    <Link to="/page-2/">Go to page 2</Link> <br />
+                    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+                </p>
+                <Helmet>
+                    <script src="https://use.typekit.net/foobar.js"></script>
+                </Helmet>
+            </Layout>
+        );
+    }
 }
 
-export default IndexPage
+export default ApiCall;
